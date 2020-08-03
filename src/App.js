36a08a13b6d4fef588330daf8a59b5570600e3d0
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-02 23:00:47 -0700

    state change management retuning, next adding class methods.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -8,30 +10,35 @@ class App extends Component {
 
     this.state = {
       monsters: [],
-      searchField: ''
-    }
+      searchField: "",
+    };
   }
-
 
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(response => response.json())
-    .then(users => this.setState( {monsters: users} ))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-  
-  render () {
-
+  render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value })} />
-        <CardList monsters={filteredMonsters} />
-      </ div>
-        )
-  
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) =>
+            this.setState({
+              searchField: e.target.value,
+            })
+          }
+        />
+        <CardList monsters={filteredMonsters} />{" "}
+      </div>
+    );
   }
 }
 
