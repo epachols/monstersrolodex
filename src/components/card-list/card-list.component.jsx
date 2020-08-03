fbffcc54842f53b18a9f18f10b80df5b46432c46
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-02 21:13:28 -0700

    imported card, successfully changed parent-child rel

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import './card-list.styles.css';
 
-export const CardList = (props) => {
+import { Card } from '../card/card.component';
 
-     return (
-       <div className="card-list">
-         {props.monsters.map((monster) => (
-           <h1 key={monster.id}>{monster.name}</h1>
-         ))}
-       </div>
-     );
+export const CardList = (props) => {
+    return (
+
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+    );
 }
 
