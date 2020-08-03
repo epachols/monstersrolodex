e9b8cc283e04f717fbba0e55be417956fd5738cc
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-02 20:39:05 -0700

    working now on the card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -21,9 +22,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   
