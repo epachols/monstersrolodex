ab3feee3a9319aeb2ea395cf6a8a66a95cede41d
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-05 09:56:34 -0700

    deploy scripts and ghpages added in for deployment

--- src/App.js
+++ src/App.js
@@ -12,12 +12,26 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+    
+    //note that the below is the way to BIND the 'this' within our custom method handleChange, even though it wouldn't normally recognize the 'this' reference for a custom method (doesn't immediately inherit)
+    //so we take the "this" inherited from the component class, and binds the handlechange 'this' to it, thus linking our custom method in properly.
+    // this.handleChange = this.handleChange.bind(this);
+    //TODO: importantly, we can use es6' tendency to SET CONTEXT to our advantage!
   }
 
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
+  }
+  
+  // handleChange(e) {
+  //   this.setState({searchField: e.target.value})
+  // }
+  //taking advantage of lexical scoping rather than binding above.
+  //because of how it defines where it belongs in lexical scoping, by using an arrow here we don't have to do this ridiculous massive contextual binding.
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
   }
 
   render() {
@@ -28,13 +42,10 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox
-          placeholder="search monsters"
-          handleChange={(e) =>
-            this.setState({
-              searchField: e.target.value,
-            })
-          }
+          placeholder="search for monsters"
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />{" "}
       </div>
